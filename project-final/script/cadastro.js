8935726873d18c405b04e591342c5f9a69021b33
Henry Modesto <modestohenry48@gmail.com>
2024-09-25 14:01:38 -0300

    FETCH(Nova versao)

--- project-final/script/cadastro.js
+++ project-final/script/cadastro.js
@@ -1,31 +1,51 @@
 const url = "https://go-wash-api.onrender.com/api/user"; 
-async function cadastroUsuario(){
 
-    let name, email, api, resposta
+async function cadastroUsuario() {
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    let cpf_cnpj = document.getElementById('cpf').value; 
+    let birthday = document.getElementById('birthday').value; 
 
-    name = document.getElementById('name').value
-    email = document.getElementById('email').value
-    api = await fetch(url,{
-        method:"POST",
-        body:JSON.stringify({
-            "name":name,
-            "email":email,
-            "user_type_id":1,
-            "password": "123456",
-            "cpf_cnpj": "00188338020",
+
+
+
+    let api = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+            "name": name,
+            "email": email,
+            "user_type_id": 1,
+            "password": password,
+            "cpf_cnpj": cpf_cnpj,
             "terms": 1,
-            "birthday":"2000-10-12"    
+            "birthday": birthday
         }),
-        headers:{
-            'Content-Type':'application/json'
+        headers: {
+            'Content-Type': 'application/json'
         }
     });
 
-    if(api.ok){
-        resposta = await api.json();
-        console.log(resposta)
-        return
-    }
+    if (api.ok) {
+        let resposta = await api.json();
+        console.log(resposta);
+        alert("Usuário cadastrado com sucesso."); 
+
+        return;
+    } 
     let respostaErro = await api.json();
-        console.log(respostaErro.data.errors.cpf_cnpj)
+
+    if(respostaErro?.data?.errors?.cpf_cnpj){
+        alert(respostaErro.data.errors.cpf_cnpj[0]);
+
+    }
+    else {
+        alert("Erro desconhecido ao tentar cadastrar o usuário.");
+    }
 }
+
+
+    
+
+
+
